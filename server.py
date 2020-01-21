7bd3df88cd982fda944cdc409db502ad361c6ac5
renmu <1101022351@qq.com>
2020-01-21 17:03:58 +0800

    add redis

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/ncov"
+
+cache = Cache(config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': "redis://localhost:6123"})
+cache.init_app(app)
 mongo = PyMongo(app)
 
 
 @app.route('/')
+@cache.cached(timeout=50)
 def hello_world():
     array = []
     for data in mongo.db.ncov.find({}):
